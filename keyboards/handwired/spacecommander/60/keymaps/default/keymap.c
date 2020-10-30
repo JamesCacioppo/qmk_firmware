eb561fc72711b4b3c646c067412afa17e6348960
James Cacioppo <james.cacioppo@wartsila.com>
2020-10-30 14:30:41 -0400

    Add Slack macros

--- keyboards/handwired/spacecommander/60/keymaps/default/keymap.c
+++ keyboards/handwired/spacecommander/60/keymaps/default/keymap.c
@@ -1,12 +1,36 @@
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    SLCK_NXT = SAFE_RANGE,
+    SLCK_PRV
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SLCK_NXT:
+            if (record->event.pressed) {
+                // when keycode SLCK_NXT is pressed
+                SEND_STRING(SS_LALT(SS_LSFT(X_DOWN)) SS_TAP(X_ESC));
+            }
+            break;
+
+        case SLCK_PRV:
+            if (record->event.pressed) {
+                // when keycode SLCK_PRV is pressed
+                SEND_STRING(SS_LALT(SS_LSFT(X_UP)) SS_TAP(X_ESX));
+            }
+            break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,      KC_BSPC,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,         KC_ENT,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT,
-        KC_DEL,  KC_CAPS, KC_LALT, KC_LGUI,  LT(1, KC_ENT), LT(2, KC_SPC),      KC_RGUI,   KC_RALT, KC_NO,   KC_NO,      KC_NO
+        KC_DEL,  KC_CAPS, KC_LALT, KC_LGUI,  LT(1, KC_ENT), LT(2, KC_SPC),      KC_RGUI,   KC_RALT, SLCK_NXT, SLCK_PRV,  KC_NO
     ),
 
     [1] = LAYOUT(
